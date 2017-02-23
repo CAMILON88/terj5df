2634a616d3e709060c18344b7c6f119ea9f7311d
Juan Gomez Aranzazu <j.gomez267@gmail.com>
2017-02-23 09:27:35 -0500

    VERSION DE PRUEBA FINALIZADA

--- src/App.js
+++ src/App.js
@@ -1,20 +1,65 @@
 import React, { Component } from 'react';
 
+
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+     
+      
+     lista: [],
+     firstName:'',
+     lastName:''
+
+
+    }
+
+  }
+ textNombre(event) {
+  
+
+    this.setState({
+      firstName: event.target.value
+  })
+  }
+
+   textApellido(event) {
+   
+    this.setState({
+      lastName: event.target.value
+  })
+  }
+
+  agregar(event)
+  {
+     
+    event.preventDefault();
+     this.setState({
+      lista: this.state.lista.concat({firstName:this.state.firstName, lastName:this.state.lastName}),
+      firstName:'',
+      lastName:''
+     
+
+        
+    })
+   
+  }
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.agregar.bind(this)}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input type="text" className="form-control" name="first-name" value={this.state.firstName} onChange={this.textNombre.bind(this)} />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input type="text" className="form-control" name="last-name" value={this.state.lastName} onChange={this.textApellido.bind(this)}/>
               </div>
 
               <div className="action">
@@ -30,7 +75,8 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+              
+             {this.state.lista.map((list, index) => <tr key={index}><td>{list.firstName}</td><td>{list.lastName}</td></tr>)}
               </tbody>
             </table>
           </div>
